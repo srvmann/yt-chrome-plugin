ab6c030d02e80fe81925d169c7931064eacc62b5
srvmann <sauravgrover567@gmail.com>
2025-10-29 11:50:47 +0530

    Corrected the tracking_uri in test_load_model file

--- scripts/test_load_model.py
+++ scripts/test_load_model.py
@@ -3,7 +3,7 @@ import pytest
 from mlflow.tracking import MlflowClient
 
 # Set your remote tracking URI
-mlflow.set_tracking_uri("hhttp://13.203.227.156:5000/")
+mlflow.set_tracking_uri("http://13.203.227.156:5000/")
 
 @pytest.mark.parametrize("model_name, stage", [
     ("yt_chrome_plugin_model", "staging"),])
